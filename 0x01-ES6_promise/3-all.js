7c6211060d1d55a818d6b5cca7c1e1ebd5cf8767
Hassanabdo1 <Hassanabdelftah@outlook.com>
2024-07-25 12:23:41 +0300

    Promes

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const photo = results[0];
+      const user = results[1];
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
+
+export default handleProfileSignup;
