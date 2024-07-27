89d6a48c242c62b5183dce6035b84bdab23dfbda
Hassanabdo1 <Hassanabdelftah@outlook.com>
2024-07-27 03:50:11 +0300

    ubdate2

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,16 +1,18 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  // Call the functions with the provided arguments
-  const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPromise = uploadPhoto(fileName);
-
-  // Use Promise.allSettled to handle both promises and return their results
-  return Promise.allSettled([signUpPromise, uploadPromise]).then(results => {
-    return results.map(result => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason
-    }));
-  });
+  /* eslint-disable */
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((result) => {
+      const profilearray = [];
+      result.forEach((index) => {
+	if (index.status !== 'fulfilled') {
+	  profilearray.push({ status: index.status, value: `Error: ${index.reason.message}` });
+	} else {
+	  profilearray.push({ status: index.status, value: index.value });
+	}
+      });
+      return profilearray;
+    });
 }
